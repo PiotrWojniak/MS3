bda33c2e831ca0c8924e16fc23b33b46f15f20a0
PiotrWojniak <wojospajder@yahoo.ie>
2021-05-17 22:28:34 +0000

    Turn off debug option

--- app.py
+++ app.py
@@ -194,4 +194,4 @@ def page_not_found(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
